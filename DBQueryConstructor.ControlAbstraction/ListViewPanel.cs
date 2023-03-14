5f21bb4420029c319c027f11ad1735a1069a0379
Даниил Шантарин <d3l0ry@yandex.ru>
2023-03-14 16:03:13 +0500

    Отдельный проект для абстрактных и общих компонентов WinForms

---
+++ DBQueryConstructor.ControlAbstraction/ListViewPanel.cs
@@ -0,0 +1,18 @@
+﻿namespace DBQueryConstructor.ControlAbstraction;
+
+/// <summary>
+/// Панель с массивом объектов выбранного наследника типа Control
+/// Тип лучше указывать, который наследуется от ViewGroupBox
+/// </summary>
+/// <remarks>Тип лучше указывать, который наследуется от ViewGroupBox</remarks>
+/// <typeparam name="PanelType"></typeparam>
+public abstract class ListViewPanel<PanelType> : TableLayoutPanel where PanelType : GroupBox
+{
+    public ListViewPanel() : base() { }
+
+    public IEnumerable<PanelType> Panels => Controls.OfType<PanelType>();
+
+    public event EventHandler DataChanged;
+
+    public void OnDataChanged() => DataChanged?.Invoke(this, EventArgs.Empty);
+}
---
+++ DBQueryConstructor.ControlAbstraction/ViewGroupBox.cs
@@ -0,0 +1,28 @@
+﻿namespace DBQueryConstructor.ControlAbstraction;
+
+public abstract class ViewGroupBox<TableModel, QueryParameter> : GroupBox where QueryParameter : new()
+{
+    private readonly TableModel _Model;
+
+    public ViewGroupBox(TableModel model) : base()
+    {
+        if (model == null)
+        {
+            throw new ArgumentNullException(nameof(model));
+        }
+
+        _Model = model;
+        Parameter = new QueryParameter();
+
+        Padding = new Padding(5);
+        Height = 50;
+    }
+
+    public TableModel Model => _Model;
+
+    public QueryParameter Parameter { get; set; }
+
+    public event EventHandler DataChanged;
+
+    public void OnDataChanged() => DataChanged?.Invoke(this, EventArgs.Empty);
+}
