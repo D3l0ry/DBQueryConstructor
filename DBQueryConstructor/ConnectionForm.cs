16991dc712eaf9f5fd377f7d527354dc213224a3
Даниил Шантарин <d3l0ry@yandex.ru>
2023-03-14 16:10:18 +0500

    1)Добавлено сохранение строк подключения к бд в реестр Windows

--- DBQueryConstructor/ConnectionForm.cs
+++ DBQueryConstructor/ConnectionForm.cs
@@ -1,29 +1,37 @@
 ﻿using System.Data.Common;
 
-using DBQueryConstructor.Properties;
+using DBQueryConstructor.DatabaseInteractions;
+
+using Microsoft.Win32.Serialization;
 
 namespace DBQueryConstructor;
 public partial class ConnectionForm : Form
 {
+    private readonly RegistrySerializer<ConnectionSetting> _Serializer;
+    private ConnectionSetting _Setting;
+
     public ConnectionForm()
     {
         InitializeComponent();
 
-        serverTextBox.Text = Settings.Default.Server;
-        databaseTextBox.Text = Settings.Default.Database;
-        userTextBox.Text = Settings.Default.Login;
-        passwordTextBox.Text = Settings.Default.Password;
+        _Serializer = new RegistrySerializer<ConnectionSetting>();
+        _Setting = _Serializer.Deserialize();
+
+        serverTextBox.Text = _Setting?.Server;
+        databaseTextBox.Text = _Setting?.Database;
+        userTextBox.Text = _Setting?.Login;
+        passwordTextBox.Text = _Setting?.Password;
     }
 
     private void OpenConnectionButton_Click(object sender, EventArgs e)
     {
-        if(string.IsNullOrWhiteSpace(serverTextBox.Text))
+        if (string.IsNullOrWhiteSpace(serverTextBox.Text))
         {
             MessageBox.Show("Введите имя сервера!", "Ошибка данных", MessageBoxButtons.OK, MessageBoxIcon.Error);
             return;
         }
 
-        if(string.IsNullOrWhiteSpace(databaseTextBox.Text))
+        if (string.IsNullOrWhiteSpace(databaseTextBox.Text))
         {
             MessageBox.Show("Введите имя базы данных!", "Ошибка данных", MessageBoxButtons.OK, MessageBoxIcon.Error);
             return;
@@ -37,31 +45,32 @@ public partial class ConnectionForm : Form
         connectionStringBuilder.Add("Integrated Security", false);
         connectionStringBuilder.Add("Trusted_Connection", false);
 
-        if(!string.IsNullOrWhiteSpace(userTextBox.Text))
+        if (!string.IsNullOrWhiteSpace(userTextBox.Text))
         {
             connectionStringBuilder.Add("User ID", userTextBox.Text);
         }
 
-        if(!string.IsNullOrWhiteSpace(passwordTextBox.Text))
+        if (!string.IsNullOrWhiteSpace(passwordTextBox.Text))
         {
             connectionStringBuilder.Add("Password", passwordTextBox.Text);
         }
 
         try
         {
+            _Setting ??= new ConnectionSetting();
+            _Setting.Server = serverTextBox.Text;
+            _Setting.Database = databaseTextBox.Text;
+            _Setting.Login = userTextBox.Text;
+            _Setting.Password = passwordTextBox.Text;
+
+            _Serializer.Serialize(_Setting);
+
             Program.UsedDatabase?.Dispose();
-            Program.UsedDatabase = new Database.UsedDatabase(connectionStringBuilder.ToString());
-
-            Settings.Default.Server = serverTextBox.Text;
-            Settings.Default.Database = databaseTextBox.Text;
-            Settings.Default.Login = userTextBox.Text;
-            Settings.Default.Password = passwordTextBox.Text;
-
-            Settings.Default.Save();
+            Program.UsedDatabase = new UsedDatabase(connectionStringBuilder.ToString());
 
             DialogResult = DialogResult.Yes;
         }
-        catch(Exception ex)
+        catch (Exception ex)
         {
             MessageBox.Show(ex.Message, "Ошибка подключения", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
---
+++ DBQueryConstructor/ConnectionSetting.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Win32;
+using Microsoft.Win32.Serialization;
+
+namespace DBQueryConstructor;
+
+[RegistrySerializable(RegistryHive.CurrentUser, @"SOFTWARE\DBQueryConstructor")]
+internal class ConnectionSetting
+{
+    public string Server { get; set; }
+
+    public string Database { get; set; }
+
+    public string Login { get; set; }
+
+    public string Password { get; set; }
+}
