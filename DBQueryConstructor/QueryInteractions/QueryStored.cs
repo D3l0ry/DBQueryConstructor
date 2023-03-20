305a2fffd8c9f8970c44bc9adce6014d6cf34834
Даниил Шантарин <d3l0ry@yandex.ru>
2023-03-20 09:09:05 +0500

    Добавлен класс для хранения графического запроса конструктора

---
+++ DBQueryConstructor/QueryInteractions/QueryStored.cs
@@ -0,0 +1,20 @@
+﻿using DBQueryConstructor.Controls.ColumnPanels;
+using DBQueryConstructor.Controls.ConditionPanels;
+using DBQueryConstructor.Controls.JoinPanels;
+using DBQueryConstructor.Controls.TablePanels;
+
+namespace DBQueryConstructor.QueryInteractions;
+internal class QueryStored
+{
+    public string Server { get; set; }
+
+    public string Database { get; set; }
+
+    public TableParameter[] Tables { get; set; }
+
+    public QueryField[] Columns { get; set; }
+
+    public ForeignTableJoin[] Joins { get; set; }
+
+    public QueryConditionParameter[] Conditions { get; set; }
+}
