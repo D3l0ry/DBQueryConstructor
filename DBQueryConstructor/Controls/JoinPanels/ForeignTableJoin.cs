b30f0a9f62ade09bdc578257f23ef55e41e31201
Даниил Шантарин <d3l0ry@yandex.ru>
2023-03-20 09:14:32 +0500

    Добавлено свойство TableName

--- DBQueryConstructor/Controls/JoinPanels/ForeignTableJoin.cs
+++ DBQueryConstructor/Controls/JoinPanels/ForeignTableJoin.cs
@@ -3,53 +3,54 @@
 using DBQueryConstructor.DatabaseInteractions.Models;
 using DBQueryConstructor.QueryInteractions;
 
-namespace DBQueryConstructor.Controls.JoinPanels
+namespace DBQueryConstructor.Controls.JoinPanels;
+
+internal class ForeignTableJoin : IComparable<ForeignTableJoin>
 {
-    internal class ForeignTableJoin : IComparable<ForeignTableJoin>
+    public int Index { get; set; }
+
+    public string TableName { get; set; }
+
+    public TableColumnModel MainColumnTable { get; set; }
+
+    public TableColumnModel JoinedColumnTable { get; set; }
+
+    public QueryJoinType Join { get; set; }
+
+    public bool Validate() => JoinedColumnTable != null && MainColumnTable != null;
+
+    public int CompareTo(ForeignTableJoin other) => Index.CompareTo(other.Index);
+
+    public override string ToString()
     {
-        public int Index { get; set; }
-
-        public TableColumnModel MainColumnTable { get; set; }
-
-        public TableColumnModel JoinedColumnTable { get; set; }
-
-        public QueryJoinType Join { get; set; }
-
-        public bool Validate() => JoinedColumnTable != null && MainColumnTable != null;
-
-        public int CompareTo(ForeignTableJoin other) => Index.CompareTo(other.Index);
-
-        public override string ToString()
+        if (MainColumnTable == null || JoinedColumnTable == null)
         {
-            if (MainColumnTable == null || JoinedColumnTable == null)
-            {
-                return string.Empty;
-            }
-
-            StringBuilder stringBuilder = new StringBuilder();
-
-            switch (Join)
-            {
-                case QueryJoinType.Join:
-                    stringBuilder.Append("JOIN ");
-                    break;
-
-                case QueryJoinType.LeftJoin:
-                    stringBuilder.Append("LEFT JOIN ");
-                    break;
-            }
-
-            string joinTableName = JoinedColumnTable.GetTableName();
-            string joinTableColumn = JoinedColumnTable.GetColumnName();
-            string mainTableColumn = MainColumnTable.GetColumnName();
-
-            stringBuilder.Append(joinTableName);
-            stringBuilder.Append(" ON ");
-            stringBuilder.Append(joinTableColumn);
-            stringBuilder.Append('=');
-            stringBuilder.Append(mainTableColumn);
-
-            return stringBuilder.ToString();
+            return string.Empty;
         }
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        switch (Join)
+        {
+            case QueryJoinType.Join:
+                stringBuilder.Append("JOIN ");
+                break;
+
+            case QueryJoinType.LeftJoin:
+                stringBuilder.Append("LEFT JOIN ");
+                break;
+        }
+
+        string joinTableName = JoinedColumnTable.GetTableName();
+        string joinTableColumn = JoinedColumnTable.GetColumnName();
+        string mainTableColumn = MainColumnTable.GetColumnName();
+
+        stringBuilder.Append(joinTableName);
+        stringBuilder.Append(" ON ");
+        stringBuilder.Append(joinTableColumn);
+        stringBuilder.Append('=');
+        stringBuilder.Append(mainTableColumn);
+
+        return stringBuilder.ToString();
     }
 }
