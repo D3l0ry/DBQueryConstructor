d1d99c540f01167d3a5ce02fabdc9f20f0cef2d1
Даниил Шантарин <d3l0ry@yandex.ru>
2023-03-20 09:19:57 +0500

    Форматирование namespace

--- DBQueryConstructor/Controls/ColumnPanels/QueryField.cs
+++ DBQueryConstructor/Controls/ColumnPanels/QueryField.cs
@@ -2,36 +2,35 @@
 
 using DBQueryConstructor.DatabaseInteractions.Models;
 
-namespace DBQueryConstructor.Controls.ColumnPanels
+namespace DBQueryConstructor.Controls.ColumnPanels;
+
+internal class QueryField
 {
-    internal class QueryField
+    public TableColumnModel Column { get; set; }
+
+    public string ColumnAlias { get; set; }
+
+    public bool Valid() => Column != null;
+
+    public override string ToString()
     {
-        public TableColumnModel Column { get; set; }
-
-        public string ColumnAlias { get; set; }
-
-        public bool Valid() => Column != null;
-
-        public override string ToString()
+        if (Column == null)
         {
-            if (Column == null)
-            {
-                return string.Empty;
-            }
-
-            StringBuilder stringBuilder = new StringBuilder();
-
-            stringBuilder.Append(Column.GetColumnName());
-
-            if (!string.IsNullOrWhiteSpace(ColumnAlias))
-            {
-                stringBuilder.Append(" AS ");
-                stringBuilder.Append('[');
-                stringBuilder.Append(ColumnAlias);
-                stringBuilder.Append(']');
-            }
-
-            return stringBuilder.ToString();
+            return string.Empty;
         }
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.Append(Column.GetColumnName());
+
+        if (!string.IsNullOrWhiteSpace(ColumnAlias))
+        {
+            stringBuilder.Append(" AS ");
+            stringBuilder.Append('[');
+            stringBuilder.Append(ColumnAlias);
+            stringBuilder.Append(']');
+        }
+
+        return stringBuilder.ToString();
     }
 }
