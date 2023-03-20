33495e5fdfd76f2834d96e2c79f999142e9148f5
Даниил Шантарин <d3l0ry@yandex.ru>
2023-03-20 09:17:01 +0500

    Форматирование namespace

--- DBQueryConstructor/Controls/DatabasePanels/TableTreeNode.cs
+++ DBQueryConstructor/Controls/DatabasePanels/TableTreeNode.cs
@@ -1,18 +1,17 @@
 ﻿using DBQueryConstructor.DatabaseInteractions.Models;
 
-namespace DBQueryConstructor.Controls.DatabasePanels
+namespace DBQueryConstructor.Controls.DatabasePanels;
+
+internal class TableTreeNode : TreeNode, IComparable<TableTreeNode>
 {
-    internal class TableTreeNode : TreeNode, IComparable<TableTreeNode>
+    public TableTreeNode(TableModel table) : base($"{table.Schema}.{table.Name}")
     {
-        public TableTreeNode(TableModel table) : base($"{table.Schema}.{table.Name}")
-        {
-            Element = table;
-            ImageKey = "datatable.png";
-            SelectedImageKey = "datatable.png";
-        }
-
-        public TableModel Element { get; private set; }
-
-        public int CompareTo(TableTreeNode other) => Text.CompareTo(other.Text);
+        Element = table;
+        ImageKey = "datatable.png";
+        SelectedImageKey = "datatable.png";
     }
+
+    public TableModel Element { get; private set; }
+
+    public int CompareTo(TableTreeNode other) => Text.CompareTo(other.Text);
 }
