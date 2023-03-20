9738a664b808bb2cd81f32f6dcd8b0907b5dc4ee
Даниил Шантарин <d3l0ry@yandex.ru>
2023-03-20 09:16:09 +0500

    1)Форматирование namespace 2)Добавлены методы для добавления и создания панели

--- DBQueryConstructor/Controls/JoinPanels/JoinListView.cs
+++ DBQueryConstructor/Controls/JoinPanels/JoinListView.cs
@@ -1,87 +1,114 @@
 ﻿using DBQueryConstructor.ControlAbstraction;
 
-namespace DBQueryConstructor.Controls.JoinPanels
+namespace DBQueryConstructor.Controls.JoinPanels;
+
+internal class JoinListView : ListViewPanel<JoinPanel>
 {
-    internal class JoinListView : ListViewPanel<JoinPanel>
+    public JoinListView() : base() { }
+
+    protected override void OnDragEnter(DragEventArgs drgevent)
     {
-        public JoinListView() : base() { }
-
-        protected override void OnDragEnter(DragEventArgs drgevent)
+        if (!drgevent.Data.GetDataPresent(typeof(TablePanel)))
         {
-            if (!drgevent.Data.GetDataPresent(typeof(TablePanel)))
-            {
-                return;
-            }
-
-            TablePanel selectedPanel = (TablePanel)drgevent.Data.GetData(typeof(TablePanel));
-
-            if (selectedPanel.Parameter)
-            {
-                return;
-            }
-
-            drgevent.Effect = DragDropEffects.Move;
+            return;
         }
 
-        protected override void OnDragDrop(DragEventArgs drgevent)
+        TablePanel selectedPanel = (TablePanel)drgevent.Data.GetData(typeof(TablePanel));
+
+        if (selectedPanel.Parameter.IsMainTable)
         {
-            TablePanel selectedTablePanel = (TablePanel)drgevent.Data.GetData(typeof(TablePanel));
-
-            bool isExists = Panels.Any(currentTablePanel => currentTablePanel.Model == selectedTablePanel);
-
-            if (isExists)
-            {
-                const string message = "Такая таблица уже добавлена в присоединение таблиц!";
-                const string title = "Ошибка добавления таблицы";
-
-                MessageBox
-                    .Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Information);
-
-                return;
-            }
-
-            JoinPanel newJoin = new JoinPanel(selectedTablePanel);
-
-            newJoin.Dock = DockStyle.Top;
-            newJoin.DataChanged += JoinDataChanged;
-
-            Controls.Add(newJoin);
+            return;
         }
 
-        protected override void OnPaint(PaintEventArgs e)
+        drgevent.Effect = DragDropEffects.Move;
+    }
+
+    protected override void OnDragDrop(DragEventArgs drgevent)
+    {
+        TablePanel selectedTablePanel = (TablePanel)drgevent.Data.GetData(typeof(TablePanel));
+
+        bool isExists = Panels.Any(currentTablePanel => currentTablePanel.Model == selectedTablePanel);
+
+        if (isExists)
         {
-            int index = 0;
+            const string message = "Такая таблица уже добавлена в присоединение таблиц!";
+            const string title = "Ошибка добавления таблицы";
 
-            foreach (JoinPanel currentPanel in Panels)
-            {
-                currentPanel.Parameter.Index = index++;
-            }
+            MessageBox
+                .Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Information);
 
-            base.OnPaint(e);
+            return;
         }
 
-        protected override void OnControlRemoved(ControlEventArgs e)
-        {
-            JoinPanel removedPanel = (JoinPanel)e.Control;
+        JoinPanel newJoin = CreateJoinPanel(selectedTablePanel);
 
-            ClearJoinMainTableColumns(removedPanel);
-            base.OnControlRemoved(e);
+        AddPanel(newJoin);
+    }
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        int index = 0;
+
+        foreach (JoinPanel currentPanel in Panels)
+        {
+            currentPanel.Parameter.Index = index++;
         }
 
-        private void JoinDataChanged(object sender, EventArgs e) => OnDataChanged();
+        base.OnPaint(e);
+    }
 
-        public void ClearJoinMainTableColumns(JoinPanel panel)
+    protected override void OnControlAdded(ControlEventArgs e)
+    {
+        JoinPanel joinPanel = (JoinPanel)e.Control;
+        joinPanel.DataChanged += JoinDataChanged;
+        joinPanel.Model.Join = joinPanel;
+
+        base.OnControlAdded(e);
+    }
+
+    protected override void OnControlRemoved(ControlEventArgs e)
+    {
+        JoinPanel removedPanel = (JoinPanel)e.Control;
+
+        ClearJoinMainTableColumns(removedPanel);
+        base.OnControlRemoved(e);
+    }
+
+    public JoinPanel CreateJoinPanel(TablePanel table)
+    {
+        if (table == null)
         {
-            string removedPanelTableName = panel.Text;
+            throw new ArgumentNullException(nameof(table));
+        }
 
-            IEnumerable<JoinPanel> joinPanels = Panels
-                .Where(currentJoinPanel =>
-                    currentJoinPanel.Parameter.MainColumnTable?.GetTableName() == removedPanelTableName);
+        JoinPanel newJoin = new JoinPanel(table);
+        newJoin.Dock = DockStyle.Top;
 
-            foreach (JoinPanel currentJoinPanel in joinPanels)
-            {
-                currentJoinPanel.JoinMainTableColumn.SelectedIndex = -1;
-            }
+        return newJoin;
+    }
+
+    public override void AddPanel(JoinPanel panel)
+    {
+        if (panel == null)
+        {
+            throw new ArgumentNullException(nameof(panel));
+        }
+
+        Controls.Add(panel);
+    }
+
+    private void JoinDataChanged(object sender, EventArgs e) => OnDataChanged();
+
+    public void ClearJoinMainTableColumns(JoinPanel panel)
+    {
+        string removedPanelTableName = panel.Text;
+        IEnumerable<JoinPanel> joinPanels = Panels
+            .Where(currentJoinPanel =>
+                currentJoinPanel.Parameter.MainColumnTable?.GetTableName() == removedPanelTableName);
+
+        foreach (JoinPanel currentJoinPanel in joinPanels)
+        {
+            currentJoinPanel.JoinMainTableColumn.SelectedIndex = -1;
         }
     }
 }
